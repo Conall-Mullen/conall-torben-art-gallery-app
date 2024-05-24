ff20680510610fe4a805f7f5b9b0af25047e3e06
Torben Jost <torbenjost@proton.me>
2024-05-24 17:57:55 +0200

    add remaining render components to art piece details

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,11 +4,11 @@ export default function Navigation({ pieces }) {
   console.log("pieces", pieces);
   return (
     <>
-      <h2>Pieces</h2>
-
-      <Link href={`./`}>Spotlight</Link>
-      <Link href={`./art-pieces`}>Art Pieces</Link>
-      <Link href={`./favourites`}>Favourites</Link>
+      ***
+      <Link href={`./`}>Spotlight //</Link>
+      <Link href={`./art-pieces`}> Art Pieces //</Link>
+      <Link href={`./favourites`}> Favourites</Link>
+      ***
     </>
   );
 }
--- src/pages/art-pieces/[slug].js
+++ src/pages/art-pieces/[slug].js
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 
 import Image from "next/image";
+import Link from "next/link";
 
 export default function PieceDetail({ pieces }) {
   const router = useRouter();
@@ -13,17 +14,19 @@ export default function PieceDetail({ pieces }) {
 
   return (
     <>
-      <h1>
-        {currentPiece.name} by {currentPiece.artist}
-      </h1>
-
       <Image
         src={currentPiece.imageSource}
         alt={currentPiece.name}
         width={currentPiece.dimensions.width / 4}
         height={currentPiece.dimensions.height / 4}
       ></Image>
-
+      <h1>{currentPiece.name}</h1>
+      <h2>{currentPiece.artist}</h2>
+      <h3>{currentPiece.year}</h3>
+      <p>{currentPiece.genre}</p>
+      <button type="button">
+        <Link href={`/art-pieces/`}>{"<"}</Link>
+      </button>
     </>
   );
 }
