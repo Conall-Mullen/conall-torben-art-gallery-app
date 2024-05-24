99e294a8ab98355623e543e6dd953f9ea11b1737
Conall O Maolain <conallomaolain1@gmail.com>
2024-05-24 16:02:28 +0200

    add navigation

---
+++ src/components/Layout/index.js
@@ -0,0 +1,10 @@
+import Navigation from "../Navigation";
+import HomePage from "@/src/pages";
+
+export default function Layout({ pieces }) {
+  return (
+    <>
+      <Navigation pieces={pieces} />
+    </>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,9 @@
-import useSWR from "swr";
-import ArtPieces from "../components/Art Pieces";
+import Layout from "../components/Layout";
 
-const URL = "https://example-apis.vercel.app/api/art";
-
-export default function HomePage() {
-  const fetcher = async (url) => {
-    const res = await fetch(url);
-
-    if (!res.ok) {
-      const error = new Error("An error occurred while fetching the data.");
-
-      error.info = await res.json();
-      error.status = res.status;
-      throw error;
-    }
-
-    return res.json();
-  };
-
-  const { data, isLoading, error } = useSWR(URL, fetcher);
-  const pieces = data;
-
-  if (isLoading) return <h1>is loading</h1>;
-
+export default function HomePage({ pieces }) {
   return (
     <>
-      <ArtPieces pieces={pieces} />
+      <Layout pieces={pieces} />
     </>
   );
 }
