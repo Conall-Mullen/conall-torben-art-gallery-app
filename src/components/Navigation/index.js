99d5e82fc882fe8561515d3e3fbdeb5e2c2d2c28
Conall O Maolain <conallomaolain1@gmail.com>
2024-05-28 15:49:01 +0200

    fix nav

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,14 +4,14 @@ export default function Navigation({ pieces }) {
   console.log("pieces", pieces);
   return (
     <footer className="nav-bar">
-      <Link href={`./`} data-testid="link-spotlight">
+      <Link href={`/`} data-testid="link-spotlight">
         Spotlight //
       </Link>
-      <Link href={`./art-pieces`} data-testid="link-art-pieces">
+      <Link href={`/art-pieces`} data-testid="link-art-pieces">
         {" "}
         Art Pieces //
       </Link>
-      <Link href={`./favourites`} data-testid="link-favorites">
+      <Link href={`/favourites`} data-testid="link-favorites">
         {" "}
         Favourites //
       </Link>
